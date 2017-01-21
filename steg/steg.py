087ca84ed1e9266072324e37d019089ab983cb51
tkadur <thejas.kadur@gmail.com>
2017-01-21 04:37:40 -0500

    Made lots of progress toward json stegging

--- steg/steg.py
+++ steg/steg.py
@@ -3,21 +3,23 @@ from jpeg_encoder import JpegEncoder
 import sys
 import os
 from jpeg_extract import JpegExtract
+import logging
 
 def encode(input, data, output = 'output.jpg', password = 'correcthorsebatterystaple', comment = 'PennApps XV', quality = 100):
-    logging.basicConfig(format='%(asctime)-15s [%(name)-9s] %(message)s', level = logging.ERROR)
+    logging.basicConfig(format='%(asctime)-15s [%(name)-9s] %(message)s', level = logging.INFO)
     image = Image.open(input)
-    data = open(data, "rb")
-    output = open(output, 'wb')
 
+    output = open(output, 'wb')
     encoder = JpegEncoder(image, int(quality), output, comment)
-    encoder.compress(data.read(), password)
+    encoder.compress(data, password)
     output.close()
 
 def decode(input, output, password):
-    out = open(output, 'wb')
+    out = sys.stdout#open(output, 'wb')
     image = open(input, 'rb')
-    JpegExtract(output, options.password).extract(image.read())
+    JpegExtract(out, password).extract(image.read())
 
     image.close()
     output.close()
+
+# encode("/Users/Thejas/Documents/PennAppsXV/jmackey.jpg", "testing testing")
